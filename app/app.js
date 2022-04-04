a849f9ff9ee603e05b8921c2ce2cebb30f542e87
FreddyAndresA <63604058+FreddyAndresA@users.noreply.github.com>
2022-04-04 15:15:55 -0500

    Agregando funcionalidad a las alertas

--- app/app.js
+++ app/app.js
@@ -1,19 +1,54 @@
-let countDownDate = new Date ("Dec 1, 2022 00:00:00").getTime();
+/* Contador de la pagina de inicio */
 
-    let x = setInterval(function(){
-        let now = new Date().getTime();
-        let distance = countDownDate - now;
+let countDownDate = new Date("May 1, 2022 00:00:00").getTime();
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
 
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24 ));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.getElementById("launch").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("launch").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-        if (distance < 0 ){
-            clearInterval(x);
-            document.getElementById("launch").innerHTML = "EXPIRED";
-        }
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("launch").innerHTML = "EXPIRED";
+    }
+}, 1000);
 
-    }, 1000);
+
+/* Alerta para el agendamiento de prueba de manejo */
+
+let testDrive = document.getElementById("testDrive");
+
+testDrive.addEventListener("click", ()=> {
+    let date = document.getElementById("date").value;
+    if(date == "") {
+        Swal.fire({
+            title: "Gracias por tu interes",
+            text: "Por favor selecciona una fecha para agendar tu prueba de manejo.",
+            icon: "warning",
+            confirmButtonText: "Seleccionar",
+            background: "#191613",
+            backdrop: false,
+            timer: 10000,
+            timerProgressBar: true,
+        })
+    }
+    else if (date != "") {
+        Swal.fire({
+            title: "Gracias por tu interes",
+            text: `Tu prueba de manejo fue agendada exitosamente para el dia ${date}`,
+            icon: "success",
+            confirmButtonText: "Seleccionar",
+            background: "#191613",
+            backdrop: false,
+            timer: 10000,
+            timerProgressBar: true,
+            footer: '<h5 class="text-center">No olvides traer tu licencia de conduccion y tu documento de identidad</h5>'
+        })
+    }
+});
+
---
+++ app/contacto.js
@@ -0,0 +1,32 @@
+let sendContact = document.getElementById("sendContact");
+
+sendContact.addEventListener("click", ()=> {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    
+    if(name == "" || email == "" || message == "") {
+        Swal.fire({
+            title: "Gracias por tu contacto",
+            text: "Por favor selecciona diligencia todos los campos para ponerte en contacto.",
+            icon: "warning",
+            confirmButtonText: "Seleccionar",
+            background: "#191613",
+            backdrop: false,
+            timer: 10000,
+            timerProgressBar: true,
+        })
+    }
+    else {
+        Swal.fire({
+            title: "Gracias por tu contacto",
+            text: "Uno de nuestros asesores revisará tu solicitud y se comunicará a la mayor brevedad.",
+            icon: "success",
+            confirmButtonText: "Seleccionar",
+            background: "#191613",
+            backdrop: false,
+            timer: 10000,
+            timerProgressBar: true,
+        })
+    }
+});
